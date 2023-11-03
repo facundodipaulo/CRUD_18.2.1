438330795b58d9b551ced14a70b532cb8b667927
MFuentes01 <megax.yt.uy@gmail.com>
2023-11-03 10:25:01 -0300

    second_commit

---
+++ scripts/init.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const ServerKey = `6544ee515a0b4b04436d3fe2`;
+    const getAll_addURL = `https://${ServerKey}.mockapi.io/users`;
+    const getOne_modify_deleteURL = `https://${ServerKey}.mockapi.io/users/:id`;
+    const btnGet1 = document.getElementById('btnGet1');
+    const inputGet1Id = document.getElementById('inputGet1Id');
+    const results = document.getElementById('results');
+
+    fetch(getAll_addURL)
+    .then
+
+    btnGet1.addEventListener('click', function() {
+        if (inputGet1Id === "") {
+
+
+
+        }
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+})
