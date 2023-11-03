9d6cccd3d6e195903cfac3c6ba944710354a6a1d
MFuentes01 <megax.yt.uy@gmail.com>
2023-11-03 11:00:15 -0300

    commit1

--- scripts/init.js
+++ scripts/init.js
@@ -7,21 +7,109 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputGet1Id = document.getElementById('inputGet1Id');
     const results = document.getElementById('results');
 
-    fetch(getAll_addURL)
-    .then
 
-    btnGet1.addEventListener('click', function() {
-        if (inputGet1Id === "") {
+    function getUser() {
+
+        const selectedId = inputGet1Id.value
+
+        fetch(getAll_addURL)
+        .then(response => response.json())
+        .then(data => {
+
+
+            if (selectedId === "" || selectedId === undefined || selectedId === null) {
+
+                data.forEach(user => {
+                results.innerHTML += `
+                <p>
+                ID: ${user.id}<br>
+                NAME: ${user.name}<br>
+                LASTNAME: ${user.lastname}<br>
+                </p>
+                `
+                });
+            } else {
+                console.log(selectedId);
+            }
+
+        })
+
+
+    }
+
+    btnGet1.addEventListener('click', getUser);
 
 
 
+/*
+
+    const options = {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(update),
+        };
+
+    
+
+    fetch(getAll_addURL, options)
+    .then(response => response.json())
+    .then(data => {
+
+
+
+
+
+    
+        //if (inputGet1Id === "") {
+
+        
+
+            data.forEach(user => {
+                results.innerHTML += `
+            <h3>${data.name}</h3>
+            `
+            });
+
+            
+
+
+
+        //}
+
+
+    
+
+})
+
+
+    const update = {
+        title: "Datos de usuario",
+        body: [nameField.value, lastnameField.value, birthdateField.value]
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(update),
+        };
+
+    fetch(DATA_URL, options)
+    .then(data => {
+        if (!data.ok) {
+            throw Error(data.status);
         }
-
-
-    })
-
-
-
+        return data.json();
+    }).then(update => {
+        console.log(update);
+    }).catch(e => {
+        console.log(e);
+    });
+});
+    }); */
 
 
 
